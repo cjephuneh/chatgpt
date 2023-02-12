ad9701aeebac9028b4728a94e4cfb612e6fecd5a
caleb jephuneh <calebjephunneh@gmail.com>
2023-02-13 01:10:51 +0300

    before

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,6 +1,10 @@
+'use client'
 import NewChat from "../components/NewChat"
+import { useSession, signOut } from 'next-auth/react'
 
 function SideBar(){
+    const {data: session } = useSession();
+    
     return(
         <div className="p-2 flex flex-col h-screen">
             <div className="flex-1">
@@ -13,6 +17,15 @@ function SideBar(){
 
 
             </div>
+            {session && (
+                <img 
+                    onClick={() => signOut()}
+                    src={session.user?.image!} 
+                    alt='Profile pic'
+                    className='h-12 w-12 rounded-full cursor-pointer mx-auto
+                    hover:opacity-50'
+                />
+            )}
         </div>
     )
 }
