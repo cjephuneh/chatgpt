1d8407dbffaa430debcd56f3828c71e3d308b468
caleb jephuneh <calebjephunneh@gmail.com>
2023-02-12 10:15:02 +0300

    layout

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import '../styles/globals.css'
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html>
+      <head />
+      <body>
+        <div className='flex'>
+          {/*Sidebar*/}
+
+          {/*ClientProvider - Notifications*/}
+        
+        
+         <div className="bg-[#343541] flex-1">{children}</div>
+
+        </div>
+        
+      </body>
+    </html>
+  )
+}
